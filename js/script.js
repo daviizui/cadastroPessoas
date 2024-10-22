876305b40bc3a61c3f72419ee160f9357ccc5468
Davi Izui <davi.izui@gmail.com>
2024-10-21 22:04:10 -0300

    não faço ideia do que está acontecendo, tentar ativar btneditar

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ function validarnome(nome) {
         alert("O nome precisa ter no mínimo 3 letras e no máximo 120 letras.Podendo conter letras, e não números.")
         return
     }
-}
+};
 
 function validarData(dataNacimentoDigitada) {
     const dataAtual = new Date();
@@ -51,18 +51,19 @@ function validarData(dataNacimentoDigitada) {
         alert('Mês inválido. Por favor, insira um mês entre 1 e 12.');
     }
     return;
-}
+};
 
 function ordenarData(dataDesordenada) {
     let dataDigitadaArray = dataDesordenada.split("-");
     let dataOrdenada = dataDigitadaArray[2] + "/" + dataDigitadaArray[1] + "/" + dataDigitadaArray[0]
     return dataOrdenada;
-}
+};
 
-function criarNovaPessoa(pessoa) {
+function criarNovaPessoa(pessoa, index) {
 
     const tr = document.createElement("tr");
     tr.classList.add("tabela-corpo");
+    tr.id = "item-lista"
 
     const tdNome = document.createElement("td");
     tdNome.classList.add("corpo-nome");
@@ -71,23 +72,46 @@ function criarNovaPessoa(pessoa) {
     const tdData = document.createElement("td");
     tdData.classList.add("corpo-data");
     tdData.textContent = pessoa.dataNacimentoDigitada
-    const tdAcoes = document.createElement("td")
-    tdAcoes.classList.add("corpo-acoes")
-    tdAcoes.textContent = "Editar"
+    const tdAcoes = document.createElement("td");
+    tdAcoes.classList.add("corpo-acoes");
+    
+    const editarBtn = document.createElement("button");
+    editarBtn.textContent = "Editar";
+    editarBtn.classList.add("btn-editar");
+    editarBtn.id = `editar-${index}`;
+    const deletarBtn  = document.createElement("button");
+    deletarBtn.textContent = "Deletar";
+    deletarBtn.classList.add("btn-deletar");
+    deletarBtn.id = `deletar-${index}`;
+    tdAcoes.appendChild(editarBtn);
+    tdAcoes.appendChild(deletarBtn);
 
-    tr.appendChild(tdNome)
-    tr.appendChild(tdData)
-    tr.appendChild(tdAcoes)
-    return tr
-}
+    tr.appendChild(tdNome);
+    tr.appendChild(tdData);
+    tr.appendChild(tdAcoes);
+    return tr;
+};
 
-function criaLinhaNaTabela(umaPessoa) {
-    const elementoPessoa = criarNovaPessoa(umaPessoa);
+function criaLinhaNaTabela(umaPessoa,index) {
+    const elementoPessoa = criarNovaPessoa(umaPessoa,index);
     table.append(elementoPessoa);
+};
+
+
+
+function editar(evento){
+    console.log(evento.target.id);
+    
 }
 
 
 
+const tabelaParaEditar = document.querySelector("#tabela")
+tabelaParaEditar.addEventListener("click", editar)
+
+
+
+  
 
 
 
